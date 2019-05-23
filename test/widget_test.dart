7f237be814252fbc9d9872ae79c3066b2d21d4bc
petermwash <petermwash48@gmail.com>
2019-05-23 17:34:56 +0300

    Add some Unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,8 +4,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_note_keeper/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets("AppBartitle", (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+    
+    expect(find.text("Note Keeper"), findsNothing);
   });
 }
